ecc1c6bc68a866582ec8cf075881602a220cb629
ph5500 <philfives@gmail.com>
2020-07-09 16:01:57 -0600

    completed sumdiff problem

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,11 @@
-# Your code here
+# method on a dictionary might be useful
+# it's possible for .sort() to sort on multiple keys at once
+# negatives might help where reverse won't
+# you can print a variable field width in an f-string with nested braces, like so {x:{y}}
 
+import pandas 
+from collections import Counter
+
+letter_counts = Counter(robin.txt)
+df = pandas.DataFrame.from_dict(letter_counts, orient='index')
+df.plot(kind='bar')
--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -11,5 +11,20 @@ q = (1, 3, 4, 7, 12)
 def f(x):
     return x * 4 + 6
 
-# Your code here
+# iterate and store set into a dictionary
 
+def Combos(orderedNumSet):
+    pass
+# list from L-R
+a = f(1) + f(3)
+# List from R-L
+b = f(12) - f(7)
+
+# compare sides 
+if a == b:
+    pass
+
+if __name__ == '__main__':
+    print(Combos)
+    
+    print("")
